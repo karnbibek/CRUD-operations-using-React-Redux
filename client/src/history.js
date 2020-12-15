a57fd87352a00d2ec4dbb4422cd0822f38a76721
Bibek <karnabibek96@gmail.com>
2020-12-15 12:43:15 +0530

    first commit

---
+++ client/src/history.js
@@ -0,0 +1,3 @@
+// import createHistory from 'history/createBrowserHistory';
+var createHistory = require('history').createBrowserHistory; // the history package was installed automatically while installing react-router-dom.
+export default createHistory();
